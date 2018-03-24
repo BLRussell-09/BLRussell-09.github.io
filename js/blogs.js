55798ce8b21125000e2ef307882ed3cddd95fab5
Barry Russell <blrussell09@gmail.com>
2018-03-24 13:38:39 -0500

    Added 4th blog post

--- js/blogs.js
+++ js/blogs.js
@@ -16,6 +16,12 @@ var blogs = [
     title: "Projects",
     date: " 3.13.18",
     post: "This week we started to work on projects with our groups. Here we got to start having fun, creating new things. We workd on a translator that would return your words to you in either Elvish, Klingon, or Dothraki. I had trouble with mine, returning a new line for every word translated. As it turns out, I needed an internal counter to make it work. I'm starting to more easily put the pieces together from what I've learned so far and use it to solve the problem. This related in a way to a problem that we previously worked on with arrays, the beggars issue. Now if only I were any good at styling the translator would look nice and work well."
+  },
+  {
+    id: "blog4",
+    title: "Travel-Diary",
+    date: " 3.24.18",
+    post: "This week we worked on the travel diary, and to be honest it's the first project that I've managed to finish in one sitting. Getting the cards to print on the page, was pretty simple, as well as adding a text box to the card. However I had a little trouble printing the new cards out. I treated the problem like spaghetti, throwing every possible solution to the wall to see what stuck. eventually I got the cards to print but no text box, and had to follow the error codes down the rabbit hole. Now my cards print appropriatly and I've started working on a delete button that doesn't work."
   }
 ]
 function prinToDom(stringToPrint,divId){
